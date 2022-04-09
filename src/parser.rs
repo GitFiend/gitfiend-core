d405e79b44e2d33614236bbcfd593451cf6cd688
Toby <sugto555@gmail.com>
2022-04-09 12:15:43 +1200

    Write map! macro.

--- src/parser.rs
+++ src/parser.rs
@@ -31,3 +31,44 @@ PARSE FAILURE AT POSITION {}:
 
   result
 }
+
+// type MapFunction<T, U> = fn(T) -> U;
+
+#[macro_export]
+macro_rules! map {
+  ($parser:expr, $function:expr) => {
+    |input: &mut Input| {
+      let result = $parser(input);
+
+      if result.is_some() {
+        Some($function(result.unwrap()))
+      } else {
+        None
+      }
+    }
+  };
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use crate::{character, word};
+
+  #[test]
+  fn test_map() {
+    let my_parser = map!(word!("omg"), |result: &str| String::from(result));
+
+    let res = parse_all(my_parser, "omg");
+
+    assert_eq!(res.unwrap(), String::from("omg"));
+  }
+
+  #[test]
+  fn test_map2() {
+    let my_parser = map!(character!('c'), |result: char| String::from(result));
+
+    let res = parse_all(my_parser, "c");
+
+    assert_eq!(res.unwrap(), String::from("c"));
+  }
+}

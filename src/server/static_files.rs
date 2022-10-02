cce6249fe60cf8ed03153a2bf026d7ebe335fbbe
Toby2 <sugto555@gmail.com>
2022-10-02 16:44:38 +1300

    Set correct content type for returned static files.

--- src/server/static_files.rs
+++ src/server/static_files.rs
@@ -1,23 +1,35 @@
-use crate::dprintln;
 use std::env;
 use std::fs::File;
 use std::path::PathBuf;
-use tiny_http::{Request, Response};
+use std::str::FromStr;
 
-pub fn handle_resource_request(request: Request) {
-  let url = request.url();
+use tiny_http::{Header, Request, Response};
 
-  if let Some(dir) = get_server_dir() {
-    let file_path = dir.join(&url[3..]);
+use crate::dprintln;
 
-    dprintln!("file_path {:?}", file_path);
+pub fn handle_resource_request(request: Request) -> Option<()> {
+  let dir = get_server_dir()?;
+  let file_path = dir.join(&request.url()[3..]);
 
-    if let Ok(file) = File::open(&file_path) {
-      let response = Response::from_file(file);
+  dprintln!("file_path {:?}", file_path);
 
-      let _ = request.respond(response);
-    }
-  }
+  let file = File::open(&file_path).ok()?;
+  let mut response = Response::from_file(file);
+
+  let content_type = get_content_type(&file_path.to_string_lossy())?;
+
+  let header = Header::from_str(&content_type).ok()?;
+  response.add_header(header);
+
+  let _ = request.respond(response);
+
+  Some(())
+}
+
+fn get_content_type(file_path: &str) -> Option<String> {
+  let guess = mime_guess::from_path(&file_path);
+
+  Some(format!("Content-Type: {}", guess.first()?))
 }
 
 fn get_server_dir() -> Option<PathBuf> {

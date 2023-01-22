cd7bd81ccb988fabca40e0af7427ff13d7589988
Toby <sugto555@gmail.com>
2023-01-22 17:01:54 +1300

    Read MERGE_HEAD file for merge conflict and return commit id instead of using a git commit to detect merge conflicts.

--- src/git/queries/wip.rs
+++ src/git/queries/wip.rs
@@ -2,30 +2,38 @@ mod create_hunks;
 pub(crate) mod wip_diff;
 mod wip_patch_parsers;
 pub(crate) mod wip_patches;
-
-use crate::git::run_git;
-use crate::git::run_git::RunGitOptions;
 use crate::server::git_request::ReqOptions;
+use std::fs;
+use std::path::Path;
 
-pub fn is_merge_in_progress(options: &ReqOptions) -> bool {
+pub fn is_merge_in_progress(options: &ReqOptions) -> Option<String> {
   let ReqOptions { repo_path } = options;
 
-  let out = run_git::run_git(RunGitOptions {
-    repo_path,
-    args: ["merge", "HEAD"],
-  });
+  read_merge_head(repo_path)
 
-  if out.is_none() {
-    let out = run_git::run_git(RunGitOptions {
-      repo_path,
-      args: ["log", "-n", "1"],
-    });
+  // let out = run_git::run_git(RunGitOptions {
+  //   repo_path,
+  //   args: ["merge", "HEAD"],
+  // });
+  //
+  // // An empty repo will fail so try another thing?
+  // if out.is_none() {
+  //   let out = run_git::run_git(RunGitOptions {
+  //     repo_path,
+  //     args: ["log", "-n", "1"],
+  //   });
+  //
+  //   if out.is_none() {
+  //     return false;
+  //   }
+  //   return true;
+  // }
+  //
+  // false
+}
 
-    if out.is_none() {
-      return false;
-    }
-    return true;
-  }
+fn read_merge_head(repo_path: &str) -> Option<String> {
+  let text = fs::read_to_string(Path::new(repo_path).join(".git").join("MERGE_HEAD")).ok()?;
 
-  false
+  Some(text.trim().to_string())
 }

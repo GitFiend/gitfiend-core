d6b409cb4f2aef2ef9d9446388249cb0b855baf0
Toby <sugto555@gmail.com>
2023-05-13 17:08:42 +1200

    Add in tauri configuration.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
 use crate::git::git_settings::set_git_env;
 use crate::git::git_version::load_git_version;
 use crate::server::requests::start_async_server;
@@ -9,8 +12,23 @@ mod parser;
 mod server;
 mod util;
 
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+#[tauri::command]
+fn greet(name: &str) -> String {
+  format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+const USE_SERVER: bool = false;
+
 fn main() {
-  set_git_env();
-  load_git_version();
-  start_async_server();
+  if USE_SERVER {
+    set_git_env();
+    load_git_version();
+    start_async_server();
+  } else {
+    tauri::Builder::default()
+      .invoke_handler(tauri::generate_handler![greet])
+      .run(tauri::generate_context!())
+      .expect("error while running tauri application");
+  }
 }

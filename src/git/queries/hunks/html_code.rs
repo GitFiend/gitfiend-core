ffdb61be7d24dbecd5a9b4e219440efe148e8606
Toby <sugto555@gmail.com>
2023-08-06 18:40:47 +1200

    Start adding margin.

--- src/git/queries/hunks/html_code.rs
+++ src/git/queries/hunks/html_code.rs
@@ -7,13 +7,34 @@ pub fn get_patch_as_html(options: &ReqHunkOptions) -> Result<String, String> {
   Ok(generate_lines(&hunk_lines))
 }
 
+// Paginate if too large?
 fn generate_lines(hunk_lines: &Vec<HunkLine>) -> String {
+  let mut margin = String::new();
   let mut lines = String::new();
 
   for hunk_line in hunk_lines {
-    lines.push_str(&hunk_line.text);
-    lines.push('\n');
+    margin.push_str(&format!(
+      "<div>{}</div><div>{}</div>",
+      to_str(hunk_line.old_num),
+      to_str(hunk_line.new_num)
+    ));
+
+    lines.push_str(&format!("<div>{}\n</div>", hunk_line.text));
+
+    // lines.push_str(&hunk_line.text);
+    // lines.push('\n');
   }
 
-  lines
+  // format!(
+  //   "<div class=\"pane\"><div>{}</div><div>{}</div></div>",
+  //   margin, lines
+  // )
+
+  format!("<div class=\"margin\">{}</div><div>{}</div>", margin, lines)
+
+  // lines
+}
+
+fn to_str(num: Option<i32>) -> String {
+  num.map(|n| n.to_string()).unwrap_or("".to_string())
 }

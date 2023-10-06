369541da75eccea3bc5f54de4b1e417e9f30b634
Toby <sugto555@gmail.com>
2023-10-06 18:00:34 +1300

    Fix bugs in code that finds branches. Don't remove prefixes and avoid duplicates.

--- src/git/queries/workspace/load_current_branch.rs
+++ src/git/queries/workspace/load_current_branch.rs
@@ -33,23 +33,22 @@ pub fn read_refs(repo_path: &str, branch_name: &str) -> R<Refs> {
 
   let path = Path::new(repo_path).join(".git").join("refs");
 
-  read_refs_inner(&path.join("heads"), branch_name, &mut refs, RefsType::Local)?;
+  read_local_refs(
+    &path.join("heads"),
+    &path.join("heads"),
+    branch_name,
+    &mut refs,
+  )?;
 
   // Sometimes remotes folder doesn't exist.
-  let _ = read_refs_inner(
+  let _ = read_remote_refs(
+    &path.join("remotes"),
     &path.join("remotes"),
     branch_name,
     &mut refs,
-    RefsType::Remote,
   );
 
   Ok(refs)
-}
-
-#[derive(Debug, Clone, Copy)]
-enum RefsType {
-  Local,
-  Remote,
 }
 
 #[derive(Debug)]
@@ -59,35 +58,56 @@ pub struct Refs {
   pub others: HashSet<String>,
 }
 
-fn read_refs_inner(
-  refs_path: &PathBuf,
+fn read_local_refs(
+  current_path: &PathBuf,
+  start_path: &PathBuf,
   branch_name: &str,
   refs_result: &mut Refs,
-  refs_type: RefsType,
 ) -> R<()> {
-  for item in read_dir(refs_path)? {
+  for item in read_dir(current_path)? {
     let path = item?.path();
 
     if path.is_file() {
       if path.to_str().unwrap_or("").ends_with(branch_name) {
-        match refs_type {
-          RefsType::Local => {
-            refs_result.local_id = Some(read_to_string(path)?.trim().to_string());
-          }
-          RefsType::Remote => {
-            refs_result.remote_id = Some(read_to_string(path)?.trim().to_string());
-          }
-        }
+        refs_result.local_id = Some(read_to_string(path)?.trim().to_string());
       } else {
         refs_result.others.insert(
-          PathBuf::from(&path.strip_prefix(refs_path)?)
+          path
+            .strip_prefix(start_path)?
             .to_str()
             .unwrap_or("")
             .to_string(),
         );
       }
     } else if path.is_dir() {
-      read_refs_inner(&path, branch_name, refs_result, refs_type)?;
+      read_local_refs(&path, start_path, branch_name, refs_result)?;
+    }
+  }
+
+  Ok(())
+}
+
+fn read_remote_refs(
+  current_path: &PathBuf,
+  start_path: &PathBuf,
+  branch_name: &str,
+  refs_result: &mut Refs,
+) -> R<()> {
+  for item in read_dir(current_path)? {
+    let path = item?.path();
+
+    if path.is_file() {
+      if path.to_str().unwrap_or("").ends_with(branch_name) {
+        refs_result.remote_id = Some(read_to_string(path)?.trim().to_string());
+      } else {
+        let p: PathBuf = path.strip_prefix(start_path)?.iter().skip(1).collect();
+        let name = p.to_str().unwrap_or("").to_string();
+        if name != "HEAD" {
+          refs_result.others.insert(name);
+        }
+      }
+    } else if path.is_dir() {
+      read_remote_refs(&path, start_path, branch_name, refs_result)?;
     }
   }
 

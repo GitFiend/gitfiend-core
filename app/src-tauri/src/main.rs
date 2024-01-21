8e7f7f9b4e5507c95ff44737c50d30096c85311e
Toby <sugto555@gmail.com>
2024-01-21 18:13:28 +1300

    Get tauri drawing served gitfiend content.

--- app/src-tauri/src/main.rs
+++ app/src-tauri/src/main.rs
@@ -1,15 +1,20 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use core_lib::handle_request::run_request;
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn greet(name: &str) -> String {
-  format!("Hello, {}! You've been greeted from Rust!", name)
+fn handle_rpc_request(name: &str, options: &str) -> Option<String> {
+  if let Ok(result) = run_request(name, options) {
+    return Some(result);
+  }
+  None
 }
 
 fn main() {
   tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![greet])
+    .invoke_handler(tauri::generate_handler![handle_rpc_request])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }

f319c1f12954a486a12e41c29c946143e214ee65
Toby <tjsuggate@gmail.com>
2024-10-04 16:34:08 +1300

    Write a new version of read_local_refs.

--- src/git/queries/workspace/load_current_branch.rs
+++ src/git/queries/workspace/load_current_branch.rs
@@ -1,10 +1,9 @@
 use crate::f;
 use crate::git::store::STORE;
+use crate::server::request_util::{ES, R};
 use std::collections::HashSet;
 use std::fs::{read_dir, read_to_string};
 use std::path::{Path, PathBuf};
-
-use crate::server::request_util::{ES, R};
 
 pub fn load_current_branch(repo_path: &str) -> R<(String, String)> {
   let repo = STORE.get_repo_path(repo_path)?;
@@ -43,6 +42,13 @@ pub fn read_refs(repo_path: &str, branch_name: &str) -> R<Refs> {
   let heads_dir = path.join("heads");
 
   read_local_refs(&heads_dir, &heads_dir, branch_name, &mut refs)?;
+  
+  let mut refs2 = Refs {
+    local_id: None, remote_id: None, others: HashSet::new()
+  };
+  read_local_refs2(&heads_dir, &heads_dir, branch_name, &mut refs2)?;
+  
+  println!("local refs match: {}", refs == refs2);
 
   let remotes_dir = path.join("remotes");
 
@@ -57,7 +63,7 @@ pub fn read_refs(repo_path: &str, branch_name: &str) -> R<Refs> {
   Ok(refs)
 }
 
-#[derive(Debug)]
+#[derive(Debug, Eq, PartialEq)]
 pub struct Refs {
   pub local_id: Option<String>,
   pub remote_id: Option<String>,
@@ -66,25 +72,39 @@ pub struct Refs {
 
 fn read_local_refs2(
   current_path: &PathBuf,
-  start_path: &PathRef,
+  start_path: &PathBuf,
   branch_name: &str,
   refs_result: &mut Refs,
 ) -> R<()> {
   for item in read_dir(current_path)? {
     let path = item?.path();
-    
+
     if path.is_dir() {
       return read_local_refs2(&path, start_path, branch_name, refs_result);
     }
     
     let file_name = path.file_name().unwrap().to_str().unwrap();
     if !file_name.starts_with(".") && file_name != "HEAD" {
-      //
+      let found_ref = get_ref_name_from_path(&path, start_path);
+      if found_ref == branch_name {
+        refs_result.local_id = Some(read_id_from_ref_file(&path)?);
+      } else {
+        refs_result.others.insert(found_ref);
+      }
     }
-   
   }
 
   Ok(())
+}
+
+fn get_ref_name_from_path(file: &Path, start_dir: &PathBuf) -> String {
+  let ref_path = file.strip_prefix(start_dir).unwrap();
+
+  ref_path
+    .iter()
+    .filter_map(|p| p.to_str())
+    .collect::<Vec<&str>>()
+    .join("/")
 }
 
 fn read_local_refs(
@@ -193,4 +213,24 @@ fn read_head_file(head_path: &PathBuf) -> R<String> {
   }
 
   Err(ES::from(&f!("Failed to parse {:?}", head_path)))
+}
+
+fn read_id_from_ref_file(file: &PathBuf) -> R<String> {
+  Ok(read_to_string(file)?.trim().to_string())
+}
+
+#[cfg(test)]
+mod tests {
+  use crate::git::queries::workspace::load_current_branch::get_ref_name_from_path;
+  use std::path::PathBuf;
+
+  #[test]
+  fn test_get_ref_name() {
+    let start: PathBuf = ["aa", "bb"].iter().collect();
+    let ref_parts: PathBuf = ["aa", "bb", "cc", "dd"].iter().collect();
+    
+    let res = get_ref_name_from_path(&ref_parts, &start);
+    
+    assert_eq!(res, "cc/dd")
+  }
 }

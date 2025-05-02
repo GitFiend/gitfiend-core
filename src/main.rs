c999ed8dd66fade893e0c4617173ddb624d7d364
Toby <tjsuggate@gmail.com>
2025-05-02 17:04:58 +1200

    Hello world counter.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use crate::git::git_settings::set_git_env;
 use crate::git::git_version::load_git_version;
 use crate::server::requests::start_async_server;
+use iced::widget::{button, text};
+use iced::Element;
+use std::thread;
 
 mod config;
 pub(crate) mod git;
@@ -9,8 +12,28 @@ mod parser;
 mod server;
 mod util;
 
-fn main() {
+fn main() -> iced::Result {
   set_git_env();
   load_git_version();
-  start_async_server();
+
+  thread::spawn(|| {
+    start_async_server();
+  });
+
+  iced::run("A cool counter", update, view)
+}
+
+#[derive(Debug, Clone)]
+enum Message {
+  Increment,
+}
+
+fn view(counter: &u64) -> Element<Message> {
+  button(text(counter)).on_press(Message::Increment).into()
+}
+
+fn update(counter: &mut u64, message: Message) {
+  match message {
+    Message::Increment => *counter += 1,
+  }
 }
